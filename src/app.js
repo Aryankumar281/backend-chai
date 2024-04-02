5c617fa88127d7c9cf8f8f57281b43f93a8d0260
Aryan Kumar <kumararyan5214@gmail.com>
2024-04-02 11:21:55 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -8,18 +8,17 @@ app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
 }))
-
-app.use(express.json({
-    limit: "16kb"
-}))
-
-app.use(express.urlencoded({
-    extended:true,
-    limit:"16kb"
-}))
-
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
-
 app.use(cookieParser())
+
+
+//routes
+import userRouter from "./routes/user.routes.js"
+
+
+//routes decalration
+app.use("/api/v1/users",userRouter)
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 //^ not goo method so try this
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import {app} from "./app.js"
 dotenv.config({
     path: './env'
 })
